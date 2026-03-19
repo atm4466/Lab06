3b1efb9d33cdf7e0cf1d2b3f88c592385add2955
admo06-hue <atm4466@rit.edu>
2026-03-19 16:09:06 -0400

    This is just the new Rodclass overriden classes comming soon!

--- src/mobiles/Rod.java
+++ src/mobiles/Rod.java
@@ -8,4 +8,22 @@ package mobiles;
  */
 public class Rod {
     // TODO
+    private String name;
+    private int cord_len;
+    private int left_len;
+    private int right_len;
+    private Node right_node;
+    private Node left_node;
+
+    public Rod(String a,int b, int c, int d, Node e, Node f){
+        name = a;
+        cord_len =b;
+        left_len = c;
+        right_len = d;
+        left_node = e;
+        right_node = f;
+
+    }
+
+    
 }
