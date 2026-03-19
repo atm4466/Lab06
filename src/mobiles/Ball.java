dd8261888ead703be8ace0aa43e03b93d2dc113c
admo06-hue <atm4466@rit.edu>
2026-03-19 16:04:07 -0400

    Worked on ball.java

--- src/mobiles/Ball.java
+++ src/mobiles/Ball.java
@@ -4,7 +4,23 @@ package mobiles;
  * Represents a ball in the mobile tree.
  *
  * @author RIT CS
- * @author YOUR NAME HERE
+ * @author Andrew Moor
  */
-public class Ball {
+public class Ball implements Node{
+    private String name;
+    private int cord_len;
+    private int radius;
+    private int weight;
+
+    public int getWeight()
+    {
+        return weight;
+    }
+    
+    public boolean isBalanced()
+    {
+        return true;
+
+    }
+
 }
